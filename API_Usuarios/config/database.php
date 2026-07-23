8e0882f451c1842dc71506f502bd9eb1a76066a6
MuninGray <lorena.pensatori2008@gmail.com>
2026-07-23 18:16:42 -0300

    Se ha modificado el archivo database.php debido a permisos

--- API_Usuarios/config/database.php
+++ API_Usuarios/config/database.php
@@ -5,7 +5,7 @@ class Database{
 private $host = "localhost";
 private $db_name = "DataByte";
 private $username ="root";
-private $password = "LuciaCarvallo!123";
+private $password = "";
 public $conn;
 
 public function getConnection(){
