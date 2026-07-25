08ce2cf534a2ce1f7ed496f463830899a4816d8b
overheated4000 <martinheredia1663@gmail.com>
2026-07-25 15:52:14 -0300

    Se ha solucionado problemas de conexion con la base de datos

--- API_Recoleccion/public/index.php
+++ API_Recoleccion/public/index.php
@@ -1,5 +1,13 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 
 require_once "../config/database.php";
 require_once "../src/RutaController.php";
